4a9554f09ebf7abc6ff59be7625839b3c4056b9a
Samgu Lee <cable8mm@gmail.com>
2024-03-03 20:37:36 +1300

    Add github actions (#3)
    * Add github actions
    
    * PHP Linting (Pint)
    
    ---------
    
    Co-authored-by: cable8mm <cable8mm@users.noreply.github.com>

--- src/Contracts/OptionParser.php
+++ src/Contracts/OptionParser.php
@@ -8,9 +8,9 @@ interface OptionParser
      * Undocumented function
      *
      * @param  string  $code  before parsing
-     * @param  string  $name good-name
-     * @param  array  $goods option-goods
-     * @param  array  $options option-good-options
+     * @param  string  $name  good-name
+     * @param  array  $goods  option-goods
+     * @param  array  $options  option-good-options
      * @return string|void code after parsing
      */
     public static function parse(string $code, string $name, array $goods, array $options);
